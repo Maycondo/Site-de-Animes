a75c33b10fc4c5a3f04f923ac3219b0fe4c71e73
Maycon Douglas Niculau dos Santos <60269933+Maycondo@users.noreply.github.com>
2023-09-21 15:21:41 -0300

    Site-de-Animes

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,30 @@
 import React from "react";
 
-const nav = () => {
-  return (
-    <nav>
-      <ul>
-        <li>TESTE-1</li>
-        
-      </ul>
-    </nav>
-  );
-};
+function Nav() {
 
-export default nav;
+  const AcessoLogin = "LOGIN";
+  const AcessoRegistre = "REGISTRE";
+
+  return (
+    <header>
+      <nav className="Barra-de-navegaçao">
+        <div>
+          <div></div>
+        </div>
+        <div></div>
+        <ul>
+          <li className="Login">
+            <a href="">{AcessoLogin}</a>
+          </li>
+          <li className="Resgistre">
+            <a href="">{AcessoRegistre}</a>
+          </li>
+        </ul>
+      </nav>
+      <h1>TODOS OS ANIMES</h1>
+      <h1>GRATIS</h1>
+    </header>
+  );
+}
+
+export default Nav;
---
+++ src/Corpo.jsx
@@ -0,0 +1,63 @@
+import React from "react";
+
+
+function CorpoSite() {
+  //ImagShones();//
+
+  const AnimesUp = 'ANIMES EM ALTA';
+  const AnimesShones = 'ANIMES SHONENS';
+  
+
+  return (
+    <div>
+      <div className="AnimesAlta">
+        <h1 id="AnimesAlta">{AnimesUp}</h1>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+
+      <div className="AnimesAlta">
+        <h1 id="AnimesAlta">{AnimesShones}</h1>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+
+      <div className="Informaçoes-de-shonen">
+        <div>
+          <img src="" alt="" />
+        </div>
+        <img src="" alt="" />
+        <p id="Texto-Shonen">
+          Houve um homem que conquistou tudo aquilo que o mundo tinha a
+          oferecer, o lendário Rei dos Piratas, Gold Roger. Capturado e
+          condenado à execução pelo Governo Mundial, suas últimas palavras
+          lançaram legiões aos mares. "Meu tesouro? Se quiserem, podem pegá-lo.
+          Procurem-no! Ele contém tudo que este mundo pode oferecer!". Foi a
+          revelação do maior tesouro, o One Piece, cobiçado por homens de todo o
+          mundo, sonhando com fama e riqueza imensuráveis... Assim começou a
+          Grande Era dos Piratas!
+        </p>
+      </div>
+      </div>
+  );
+
+  /*function informaShonen() {
+    const Onepiece = [
+      Texto`Houve um homem que conquistou tudo aquilo que o mundo tinha a
+      oferecer, o lendário Rei dos Piratas, Gold Roger. Capturado e
+      condenado à execução pelo Governo Mundial, suas últimas palavras
+      lançaram legiões aos mares. "Meu tesouro? Se quiserem, podem pegá-lo.
+      Procurem-no! Ele contém tudo que este mundo pode oferecer!". Foi a
+      revelação do maior tesouro, o One Piece, cobiçado por homens de todo o
+      mundo, sonhando com fama e riqueza imensuráveis... Assim começou a
+      Grande Era dos Piratas!`,
+    ];
+  }
+  */
+}
+
+export default CorpoSite;
