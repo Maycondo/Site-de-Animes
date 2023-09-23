8cb05a3aa53c24736c5860ce8589c97fe199f305
Maycon Douglas Niculau dos Santos <60269933+Maycondo@users.noreply.github.com>
2023-09-23 17:47:51 -0300

    Site-de-Animes

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,40 @@
 import React from "react";
 
-function Nav() {
-
+export default function LupaAnimes() {
   const AcessoLogin = "LOGIN";
   const AcessoRegistre = "REGISTRE";
+  const TituloName = "SEJA-BEM VINDO !";
+
 
   return (
-    <header>
-      <nav className="Barra-de-navegaçao">
+    <div className="LupaAnimes">
+      <header>
+        <nav className="Barra-de-navegaçao">
+          <div>
+            <div></div>
+          </div>
+          <div></div>
+          <ul>
+            <li className="Login">
+              <a href="">{AcessoLogin}</a>
+            </li>
+            <li className="Resgistre">
+              <a href="">{AcessoRegistre}</a>
+            </li>
+          </ul>
+        </nav>
+        <h1 id="SEJA-BEM-VINDO">{TituloName}</h1>
+        <h1 id="Gratis">LupaAnimes</h1>
+      </header>
+      <main className="Animes-Class">
+        <h1>ANIMES EM ALTA</h1>
         <div>
           <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
         </div>
-        <div></div>
-        <ul>
-          <li className="Login">
-            <a href="">{AcessoLogin}</a>
-          </li>
-          <li className="Resgistre">
-            <a href="">{AcessoRegistre}</a>
-          </li>
-        </ul>
-      </nav>
-      <h1>TODOS OS ANIMES</h1>
-      <h1>GRATIS</h1>
-    </header>
+      </main>
+    </div>
   );
 }
-
-export default Nav;
