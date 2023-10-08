f410acfbd34a79d255b0a886fcb3f013765e959a
Maycon Douglas Niculau dos Santos <60269933+Maycondo@users.noreply.github.com>
2023-10-08 18:33:22 -0300

    Site-de-Animes

--- src/App.jsx
+++ src/App.jsx
@@ -1,57 +1,59 @@
 import React from "react";
 
+
 export default function LupaAnimes() {
-  const AcessoLogin = "LOGIN";
-  const AcessoRegistre = "REGISTRE";
-  const TituloName = "TODOS ANIMES!";
-
+    const AcessoLogin = "LOGIN";
+    const AcessoRegistre = "REGISTRE";
+    const TituloName = "TODOS ANIMES!";
   
-
-  return (
-    <div className="LupaAnimes">
-      <header>
-        <nav className="Barra-de-navegaçao">
-          <div className="Icone-lupa">
-            <div className="Icone-Centro"></div>
-            <div className="Icone-perna"></div>
+    
+    return (
+      <div className="LupaAnimes">
+        <header>
+          <nav className="Barra-de-navegaçao">
+            <div className="Icone-lupa">
+              <div className="Icone-Centro"></div>
+              <div className="Icone-perna"></div>
+            </div>
+            <div className="">
+              <input className="Input" placeholder="Pesquise"></input>
+              <div><button></button><i class="fa-solid fa-magnifying-glass"></i></div>
+            </div>
+            <button className="Mode-Dark">
+              <i class="fa-solid fa-moon"></i>
+            </button>
+            <ul className="Login-Registre">
+              <li>
+                <a href="">{AcessoLogin}</a>
+              </li>
+              <li className="Resgistre">
+                <a href="">{AcessoRegistre}</a>
+              </li>
+            </ul>
+          </nav>
+          <h1 id="SEJA-BEM-VINDO">{TituloName}</h1>
+          <h1 id="Gratis">GRATIS</h1>
+        </header>
+        <main className="Animes-Class">
+          <div className="Cardes-1">
+            <h1>ANIMES EM ALTA</h1>
+            <button className=""></button>
+            <div id="Cardes"></div>
+            <div id="Cardes"></div>
+            <div id="Cardes"></div>
+            <div id="Cardes"></div>
+            <button className="Next-Animes"></button>
           </div>
-          <div>
-            <input className="Input" placeholder="Pesquise"></input>
-            <div></div>
+          <div className="Cardes-2">
+            <h1>ANIMES SHONENS</h1>
+            <button className=""></button>
+            <div id="Cardes"></div>
+            <div id="Cardes"></div>
+            <div id="Cardes"></div>
+            <div id="Cardes"></div>
+            <button className="Next-Animes"></button>
           </div>
-          <button className="Mode-Dark"><i class="fa-solid fa-moon"></i></button>
-          <ul className="Login-Registre">
-            <li>
-              <a href="">{AcessoLogin}</a>
-            </li>
-            <li className="Resgistre">
-              <a href="">{AcessoRegistre}</a>
-            </li>
-          </ul>
-        </nav>
-        <h1 id="SEJA-BEM-VINDO">{TituloName}</h1>
-        <h1 id="Gratis">GRATIS</h1>
-      </header>
-      <main className="Animes-Class">
-        <div className="Cardes-1">
-        <h1>ANIMES EM ALTA</h1>
-          <button className=""></button>
-          <div id="Cardes"></div>
-          <div id="Cardes"></div>
-          <div id="Cardes"></div>
-          <div id="Cardes"></div>
-          <button className="Next-Animes"></button>
-        </div>
-        <div className="Cardes-2">
-        <h1>ANIMES SHONENS</h1> 
-          <button className=""></button>
-          <div id="Cardes"></div>
-          <div id="Cardes"></div>
-          <div id="Cardes"></div>
-          <div id="Cardes"></div>
-          <button className="Next-Animes"></button>
-        </div>
-      </main>
-    </div>
-  );
-}
+        </main>
+      </div>
+    );
+  }
--- src/components/Textos.jsx
+++ src/components/Textos.jsx
@@ -1,3 +1,19 @@
+import React from "react";
+
+const BollCentro = document.querySelector(".Icone-Centro")
+BollCentro.addEventListener('')
+
+const updateBollposition = (e) => {
+
+    const mouseX = e.clientX; 
+    const mouseY = et.clientY;
+    const centerX = window.innerWidth / 2;
+    const centerY = window.innerHeight / 2;
+    
 
 
-const
+
+
+}
+
+export default updateBollposition
